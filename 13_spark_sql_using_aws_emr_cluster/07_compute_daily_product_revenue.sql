7aee71204d61f40093989bbb4f46cccc09af8a3f
dgadiraju <dgadiraju@itversity.com>
2022-07-19 13:13:00 +0000

    Spark SQL Scripts

---
+++ 13_spark_sql_using_aws_emr_cluster/07_compute_daily_product_revenue.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS retail_ods;
+USE retail_ods;
+
+CREATE TEMPORARY VIEW orders
+USING JSON
+OPTIONS (path='s3://airetail/retail_db_json/orders');
+
+CREATE TEMPORARY VIEW order_items
+USING JSON
+OPTIONS (path='s3://airetail/retail_db_json/order_items');
+
+INSERT OVERWRITE DIRECTORY 's3://airetail/retail_db_json/daily_product_revenue'
+USING JSON
+SELECT o.order_date,
+    o.order_status,
+    oi.order_item_product_id,
+    round(sum(oi.order_item_subtotal), 2) AS revenue
+FROM orders AS o JOIN order_items AS oi
+  ON o.order_id = oi.order_item_order_id
+GROUP BY o.order_date,
+    o.order_status,
+    oi.order_item_product_id
+ORDER BY o.order_date,
+    revenue DESC;
